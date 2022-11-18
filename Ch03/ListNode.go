e12d972929d17a67e352d867ad7d9bbe0cd90e61
sleepynut <nut.pa@kbtg.tech>
2022-11-18 21:16:35 +0700

    Implement RandListNode
    RandListNode is normal ListNode plus the additional link. Given i->j is a so-called addition link, where i < j, there exists at least one addition link for the ListNode to become RandListNode.

--- Ch03/ListNode.go
+++ Ch03/ListNode.go
@@ -7,6 +7,12 @@ type ListNode struct {
 	Next *ListNode
 }
 
+type RandListNode struct {
+	Val  int
+	Next *RandListNode
+	Rand *RandListNode
+}
+
 func GenLinkList(n int) (*ListNode, *ListNode) {
 	if n <= 0 {
 		return nil, nil
@@ -18,6 +24,30 @@ func GenLinkList(n int) (*ListNode, *ListNode) {
 	}
 	for i := 0; i < n-1; i++ {
 		temp[i].Next = temp[i+1]
+	}
+
+	return temp[0], temp[n-1]
+}
+
+func GenRandLinkList(n int, rm map[int]int) (*ListNode, *ListNode) {
+	if n <= 0 {
+		return nil, nil
+	}
+
+	temp := make([]*ListNode, n)
+	for i := 0; i < n; i++ {
+		temp[i] = &ListNode{Val: i}
+	}
+	for i := 0; i < n-1; i++ {
+		temp[i].Next = temp[i+1]
+	}
+
+	for k, v := range rm {
+		if k >= v || k < 0 || v < 0 || k >= n || v >= n {
+			return nil, nil
+		}
+
+		temp[k].Next = temp[v]
 	}
 
 	return temp[0], temp[n-1]
@@ -63,4 +93,15 @@ func PrintList(head *ListNode) string {
 		head = head.Next
 	}
 	return out
+}
+
+func PrintRandList(head *RandListNode) string {
+	if head == nil {
+		return ""
+	}
+
+	s := strconv.Itoa(head.Val) + " "
+	s += PrintRandList(head.Rand)
+	s += PrintRandList(head.Next)
+	return s
 }
